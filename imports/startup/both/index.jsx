8533d47f475b0a72a26971d354a10bb06c7db05e
datamaker <datamaker@urbanbase.com>
2016-12-22 11:50:05 +0900

    코드 정리

--- imports/startup/both/index.jsx
+++ imports/startup/both/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import { IndexRoute, Route, Link } from 'react-router';
-import { ReactRouterSSR } from 'meteor/datamaker:react-router-ssr';
-import { ReactMeteorData } from 'react-meteor-data';
-import { configureStore } from './store';
+import {Provider} from 'react-redux';
+import {IndexRoute, Route, Link} from 'react-router';
+import {ReactRouterSSR} from 'meteor/datamaker:react-router-ssr';
+import {ReactMeteorData} from 'react-meteor-data';
+import {configureStore} from './store';
 import ReactHelmet from 'react-helmet';
 import ReactCookie from 'react-cookie';
 //import ReactMixin from 'react-mixin';
@@ -33,34 +33,34 @@ let App = React.createClass({
                     titleTemplate="%s | meteor-router-ssr-demo"
                     defaultTitle="meteor-router-ssr-demo"
                     meta={[
-                        { charset : "utf-8" },
-                        { name: 'viewport', content: 'width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0' },
-                        { name: "theme-color", content: "#ffffff" },
-                        { name: "msapplication-TileColor", content: "#ffffff" },
-                        { name: "msapplication-TileImage", content: "https://dummyimage.com/114x114/000/fff" },
-                        { name: "keywords", content: 'react, redux, react-router-ssr, ssr' },
-                        { name: 'description', content: 'Meteor 1.4 Universal Rendering Demo' },
-                        { property: "og:site_name", content: "meteor-router-ssr-demo" },
-                        { property: "og:title", content: "Meteor 1.4 Universal Rendering Demo" },
-                        { property: "og:type", content: "article" },
-                        { property: "og:url", content: process.env.ROOT_URL },
-                        { property: "og:image", content: "https://dummyimage.com/600x400/000/fff&text=datamaker/meteor-router-ssr-demo" },
-                        { property: "og:description", content: "Meteor 1.4 Universal Rendering Demo" },
+                        {charset: "utf-8"},
+                        {name: "viewport", content: "width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0"},
+                        {name: "theme-color", content: "#ffffff"},
+                        {name: "msapplication-TileColor", content: "#ffffff"},
+                        {name: "msapplication-TileImage", content: "https://dummyimage.com/114x114/000/fff"},
+                        {name: "keywords", content: "react, redux, react-router-ssr, ssr"},
+                        {name: "description", content: "Meteor 1.4 Universal Rendering Demo"},
+                        {property: "og:site_name", content: "meteor-router-ssr-demo"},
+                        {property: "og:title", content: "Meteor 1.4 Universal Rendering Demo"},
+                        {property: "og:type", content: "article"},
+                        {property: "og:url", content: process.env.ROOT_URL},
+                        {property: "og:image", content: "https://dummyimage.com/600x400/000/fff&text=datamaker/meteor-router-ssr-demo"},
+                        {property: "og:description", content: "Meteor 1.4 Universal Rendering Demo"},
                     ]}
                     link={[
-                        { rel: "apple-touch-icon", sizes: "57x57", href: "https://dummyimage.com/57x57/000/fff" },
-                        { rel: "apple-touch-icon", sizes: "60x60", href: "https://dummyimage.com/60x60/000/fff" },
-                        { rel: "apple-touch-icon", sizes: "72x72", href: "https://dummyimage.com/72x72/000/fff" },
-                        { rel: "apple-touch-icon", sizes: "76x76", href: "https://dummyimage.com/76x76/000/fff" },
-                        { rel: "apple-touch-icon", sizes: "114x114", href: "https://dummyimage.com/114x114/000/fff" },
-                        { rel: "apple-touch-icon", sizes: "120x120", href: "https://dummyimage.com/120x120/000/fff" },
-                        { rel: "apple-touch-icon", sizes: "144x144", href: "https://dummyimage.com/144x144/000/fff" },
-                        { rel: "apple-touch-icon", sizes: "152x152", href: "https://dummyimage.com/152x152/000/fff" },
-                        { rel: "apple-touch-icon", sizes: "180x180", href: "https://dummyimage.com/180x180/000/fff" },
-                        { rel: "icon", type: "image/png", sizes: "192x192", href: "https://dummyimage.com/192x192/000/fff" },
-                        { rel: "icon", type: "image/png", sizes: "32x32", href: "https://dummyimage.com/32x32/000/fff" },
-                        { rel: "icon", type: "image/png", sizes: "96x96", href: "https://dummyimage.com/96x96/000/fff" },
-                        { rel: "icon", type: "image/png", sizes: "16x16", href: "https://dummyimage.com/16x16/000/fff" },
+                        {rel: "apple-touch-icon", sizes: "57x57", href: "https://dummyimage.com/57x57/000/fff"},
+                        {rel: "apple-touch-icon", sizes: "60x60", href: "https://dummyimage.com/60x60/000/fff"},
+                        {rel: "apple-touch-icon", sizes: "72x72", href: "https://dummyimage.com/72x72/000/fff"},
+                        {rel: "apple-touch-icon", sizes: "76x76", href: "https://dummyimage.com/76x76/000/fff"},
+                        {rel: "apple-touch-icon", sizes: "114x114", href: "https://dummyimage.com/114x114/000/fff"},
+                        {rel: "apple-touch-icon", sizes: "120x120", href: "https://dummyimage.com/120x120/000/fff"},
+                        {rel: "apple-touch-icon", sizes: "144x144", href: "https://dummyimage.com/144x144/000/fff"},
+                        {rel: "apple-touch-icon", sizes: "152x152", href: "https://dummyimage.com/152x152/000/fff"},
+                        {rel: "apple-touch-icon", sizes: "180x180", href: "https://dummyimage.com/180x180/000/fff"},
+                        {rel: "icon", type: "image/png", sizes: "192x192", href: "https://dummyimage.com/192x192/000/fff"},
+                        {rel: "icon", type: "image/png", sizes: "32x32", href: "https://dummyimage.com/32x32/000/fff"},
+                        {rel: "icon", type: "image/png", sizes: "96x96", href: "https://dummyimage.com/96x96/000/fff"},
+                        {rel: "icon", type: "image/png", sizes: "16x16", href: "https://dummyimage.com/16x16/000/fff"},
                     ]}
                     script={[
                         {"src": "https://maps.googleapis.com/maps/api/js?key=xxxxxxxxxxxxxxxxx&libraries=places&region=KR", "type": "text/javascript"},
@@ -80,15 +80,17 @@ let App = React.createClass({
     }
 });
 
-class Another extends React.Component {
-    constructor() {
-        super();
-    }
+let Another = React.createClass({
+
+    getInitialState: function () {
+        return {};
+    },
 
     render() {
         return <div> Go to: <Link to="/">Home</Link>|<a href="/">Home (full reload)</a></div>
     }
-}
+
+});
 
 let Home = React.createClass({
 
@@ -100,18 +102,18 @@ let Home = React.createClass({
         };
     },
 
-    getInitialState: function() {
+    getInitialState: function () {
         return {};
     },
 
     _addOne() {
-        Items.insert({title: `test ${Math.round(Math.random()*100)}`})
+        Items.insert({title: `test ${Math.round(Math.random() * 100)}`});
         console.log('new')
     },
 
     _remove(ev) {
-        Items.remove(ev.target.id)
-        console.log('remove '+ev.target.id)
+        Items.remove(ev.target.id);
+        console.log('remove ' + ev.target.id);
     },
 
     render() {
@@ -120,7 +122,7 @@ let Home = React.createClass({
 
             {
                 this.data.items.map((item) => {
-                    return <h4 key={item._id} id={item._id} onClick={this._remove} >{item.title}</h4>
+                    return <h4 key={item._id} id={item._id} onClick={this._remove}>{item.title}</h4>
                 })
             }
 
@@ -135,8 +137,8 @@ let Home = React.createClass({
 /// Universal Rendering
 let routes = (
     <Route component={App}>
-        <Route path="/" component={Home} />
-        <Route path="another" component={Another} />
+        <Route path="/" component={Home}/>
+        <Route path="another" component={Another}/>
     </Route>
 );
 
@@ -148,14 +150,14 @@ let initialState;
 const props = {
     onUpdate() {
         // Notify the page has been changed to Google Analytics
-        ga( 'send', 'pageview' );
+        ga('send', 'pageview');
     }
 };
 
 // Create a react-helmat
-const htmlHook = ( html ) => {
+const htmlHook = (html) => {
     const head = ReactHelmet.rewind();
-    return html.replace('<head>', '<head>' + head.title + head.base + head.meta + head.link + head.script + head.noscript );
+    return html.replace('<head>', '<head>' + head.title + head.base + head.meta + head.link + head.script + head.noscript);
 };
 
 // Create a react-cookie
@@ -178,8 +180,8 @@ const wrapperHook = app => {
     return <Provider store={store}>{app}</Provider>;
 };
 
-const clientOptions = { props, historyHook, rehydrateHook, wrapperHook };
-const serverOptions = { htmlHook, preRender, historyHook, dehydrateHook };
+const clientOptions = {props, historyHook, rehydrateHook, wrapperHook};
+const serverOptions = {htmlHook, preRender, historyHook, dehydrateHook};
 
 ReactRouterSSR.Run(routes, clientOptions, serverOptions);
 
